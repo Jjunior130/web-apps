3d296a2ee23970ef49b688eca74a2311e39efdde
Junior <Jjunior130@gmail.com>
2022-11-07 07:48:30 -0500

    improve

--- src/cljs/web_apps/chat.cljs
+++ src/cljs/web_apps/chat.cljs
@@ -7,18 +7,20 @@
     [web-apps.setter :as setter]))
 
 (defn message-list []
-  (fn []
-    [:ul
-     (for [[i [message t username]]
-           (take-last 10
-             (map-indexed vector
-               (sort-by second @(rf/subscribe
-                                  [::getter/messages]))))]
-       ^{:key i}
-       [:li (getter/h-mm-ss t)
-        " - "
-        username ": "
-        message])]))
+  [re-com.core/v-box
+   :children
+   (for [[i [message t username]]
+         (take-last 10
+           (map-indexed vector
+             (sort-by second @(rf/subscribe
+                                [::getter/messages]))))]
+     ^{:key i}
+     [re-com.core/h-box
+      :children
+      [(getter/h-mm-ss t)
+       " - "
+       username ": "
+       message]])])
 
 (defn message-input
   "type in a message and send it to the server.
@@ -29,17 +31,23 @@
   []
   (let [value (reagent.core/atom nil)]
     (fn []
-      [:input.form-control
-       {:type        :text
-        :placeholder "type in a message and press enter"
-        :value       @value
-        :on-change
-        #(reset! value (-> % .-target .-value))
-        :on-key-down
-        #(when (= (.-keyCode %) 13)
-           (when (not-every? (fn [c] (= c " ")) @value)
-             (rf/dispatch [:input/on-key-down @value]))
-           (reset! value nil))}])))
+      [re-com.core/h-box
+       :children
+       [@(rf/subscribe [::getter/now-h:mm:ss])
+        " - "
+        @(rf/subscribe [::getter/username @(rf/subscribe [::getter/session-id])])
+        ": "
+        [:input.form-control
+         {:type        :text
+          :placeholder "type in a message and press enter"
+          :value       @value
+          :on-change
+          #(reset! value (-> % .-target .-value))
+          :on-key-down
+          #(when (= (.-keyCode %) 13)
+             (when (not-every? (fn [c] (= c " ")) @value)
+               (rf/dispatch [:input/on-key-down @value]))
+             (reset! value nil))}]]])))
 
 (defonce now
   (js/setInterval
@@ -49,17 +57,9 @@
 
 (defn chat-page []
   [:section.section>div.container>div.content
-   [:div.container
-    [:div.row
-     [:div.col-md-12
-      [:h2 "Welcome to chat"]]]
-    [:div.row
-     [:div.col-sm-6
-      [message-list]]]
-    [:div.row
-     [:div.col-sm-6
-      @(rf/subscribe [::getter/now-h:mm:ss])
-      " - "
-      @(rf/subscribe [::getter/username @(rf/subscribe [::getter/session-id])])
-      ": "
-      [message-input]]]]])
+   [re-com.core/v-box
+    :children
+    [[:h2 "Welcome to chat"]
+     [message-list]
+     [message-input]]]])
+
